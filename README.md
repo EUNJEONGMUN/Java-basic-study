4d4c24132653ccae620bc1a54a3162935633769b
EUNJEONGMUN <munej26@gmail.com>
2022-07-06 22:51:12 +0900

    [ADD] complete 220706 commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Java-basic-study
+
+
---
+++ src/baekjoon/iteration/boj_8393/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.iteration.boj_8393;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int res = 0;
+        for (int i=1; i<n+1; i++) {
+            res += i;
+        }
+        System.out.println(res);
+
+    }
+}

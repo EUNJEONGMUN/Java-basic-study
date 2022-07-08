2ab99aae38923cdc80e2f97c9b683ab64ffdc914
EUNJEONGMUN <munej26@gmail.com>
2022-07-08 16:49:45 +0900

    [ADD] complete 220707 commit

---
+++ src/baekjoon/iteration/boj_11022/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.iteration.boj_11022;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt();
+
+        for (int i=1; i<=tc; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+        }
+
+        sc.close();
+    }
+}

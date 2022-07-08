9475fdd1d2ab1fb4f26e231f5f942409e5defe5d
EUNJEONGMUN <munej26@gmail.com>
2022-07-08 22:57:09 +0900

    [ADD] study abstraction and inheriance

---
+++ src/spring_basic/ch3/inheritance02/날수있는.java
@@ -0,0 +1,5 @@
+package spring_basic.ch3.inheritance02;
+
+public interface 날수있는 {
+    void fly();
+}
---
+++ src/spring_basic/ch3/inheritance03/Animal.java
@@ -0,0 +1,9 @@
+package spring_basic.ch3.inheritance03;
+
+public class Animal {
+    public String name;
+
+    public void showName() {
+        System.out.printf("안녕 나는 %s야. 반가워\n", name);
+    }
+}

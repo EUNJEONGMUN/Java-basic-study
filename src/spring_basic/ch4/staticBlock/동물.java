00f305a5ef41d0080b62e0fb2a7b7c8c5f6da56d
EUNJEONGMUN <munej26@gmail.com>
2022-07-10 16:35:06 +0900

    [ADD] 220710 commit

--- src/spring_basic/ch4/staticBlock/동물.java
+++ src/spring_basic/ch4/staticBlock/동물.java
@@ -1,2 +1,7 @@
-package spring_basic.ch4.staticBlock;public class 동물 {
+package spring_basic.ch4.staticBlock;
+
+public class 동물 {
+    static {
+        System.out.println("동물 클래스 레디 온!");
+    }
 }

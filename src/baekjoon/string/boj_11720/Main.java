0f1be588bde4f4c00ef1736c6fec84cf85124c02
EUNJEONGMUN <munej26@gmail.com>
2022-07-26 15:34:54 +0900

    [ADD] complete 220726 commit

---
+++ src/baekjoon/string/boj_11720/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.string.boj_11720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        sc.close();
+
+        int res = 0;
+        for (int i=0; i<n; i++) {
+            res+=s.charAt(i)-'0';
+        }
+        System.out.println(res);
+    }
+}
---
+++ src/baekjoon/string/boj_2098/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.string.boj_2098;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        sc.close();
+
+        String newA = "";
+        String newB = "";
+        for (int i=a.length()-1; i>=0; i--) {
+            newA += a.charAt(i);
+            newB += b.charAt(i);
+        }
+
+        if (Integer.parseInt(newA)<Integer.parseInt(newB)) {
+            System.out.println(newB);
+        } else {
+            System.out.println(newA);
+        }
+
+    }
+}

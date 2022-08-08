1210d3b561a5b6ffccd0a8aae2c98eabe48e7acf
EUNJEONGMUN <munej26@gmail.com>
2022-08-08 14:15:39 +0900

    [ADD] complete 220808 commit

---
+++ src/codingTest/string/boj_16171/Main.java
@@ -0,0 +1,44 @@
+package codingTest.string.boj_16171;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String k = sc.next();
+        String new_s = "";
+        sc.close();
+
+        int left = 0;
+        int right = 0;
+        int idx = 0;
+
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i)>='A' && s.charAt(i)<='z') {
+                new_s+=s.charAt(i);
+            }
+        }
+
+        while (right<new_s.length()) {
+            if (new_s.charAt(right) == k.charAt(idx)) {
+                idx++;
+                right++;
+            } else {
+                left++;
+                while(left<new_s.length() && new_s.charAt(left) != k.charAt(0)) {
+                    left++;
+                }
+                right = left;
+                idx=0;
+            }
+
+        }
+
+        if (idx==k.length()) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/codingTest/string/boj_6550/Main.java
@@ -0,0 +1,37 @@
+package codingTest.string.boj_6550;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+            String s = st.nextToken();
+            String t = st.nextToken();
+
+            int idx = 0;
+            for (int i=0; i<t.length(); i++) {
+                if (s.charAt(idx) == t.charAt(i)) {
+                    idx++;
+                }
+                if (idx==s.length()) {
+                    break;
+                }
+            }
+
+            if (s.length()==idx) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+
+}

e1c8fab356a0b3564dfea7201d86a7019e85bd64
EUNJEONGMUN <munej26@gmail.com>
2023-01-02 20:19:02 +0900

    add: boj 15649 N과 M (1) 구현 완료

--- src/codingTest/BOJ_15649.java
+++ src/codingTest/BOJ_15649.java
@@ -1,2 +1,46 @@
-package codingTest;public class BOJ_15649 {
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15649 {
+    static int N, M;
+    static boolean[] visited;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        answer = new int[M];
+
+        solution(0);
+        System.out.println(sb.toString());
+
+    }
+
+    public static void solution(int depth) {
+        if (depth == M) {
+            for (int val : answer) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                answer[depth] = i+1;
+                solution(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
 }

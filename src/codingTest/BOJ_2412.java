c8d73c70babc26ec2029192e3ef2f315f7a37e8e
EUNJEONGMUN <munej26@gmail.com>
2023-01-13 22:32:49 +0900

    add: boj 2412 풀이중

---
+++ src/codingTest/BOJ_2412.java
@@ -0,0 +1,41 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_2412 {
+    static List<Point> points = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            points.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+    }
+
+    static class Point {
+        final int x;
+        final int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean canMove(Point point) {
+            if (Math.abs(this.x - point.x) <= 2 && Math.abs(this.y - point.y) <= 2) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
